e5f8ff6ddf6de2406fd091b30644a6628c3a0795
aram_v <aram_v@instigatemobile.com>
2020-05-06 14:06:46 +0400

    Added test

---
+++ test.js
@@ -0,0 +1,10 @@
+const chai = require('chai');
+
+chai.should();
+
+describe('Utils', function () {
+    it('should return correct structured error', function () {
+        let a = 5;
+        a.should.be.deep.equal(5);
+    });
+});
